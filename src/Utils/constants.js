a7c93134f6727423a3b16efdd4cb279b8aae83a0
puneet aggarwal <aggarwal.puneet222@gmail.com>
2019-10-12 17:04:06 +0530

    Added action types | constants

---
+++ src/Utils/constants.js
@@ -0,0 +1,6 @@
+export const UPCOMING = "upcoming";
+export const LIVE = "live";
+export const COMPLETED = "completed";
+export const ALL = "All";
+export const INTERNATIONAL = "International";
+export const DOMESTIC = "Domestic";
--- src/context/types.js
+++ src/context/types.js
@@ -1,1 +1,3 @@
 export const GET_DATA = "GET_DATA";
+export const CHANGE_STATUS = "CHANGE_STATUS";
+export const CHANGE_TYPE = "CHANGE_TYPE";
