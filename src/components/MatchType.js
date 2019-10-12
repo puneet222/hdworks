dd16997f778915852bbd8f00ab818ef344c90352
puneet aggarwal <aggarwal.puneet222@gmail.com>
2019-10-12 17:30:57 +0530

    added function

--- src/components/MatchType.js
+++ src/components/MatchType.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import CricketContext from "../context/cricketContext";
+import * as CONST from "../Utils/constants";
 
 const MatchType = () => {
+  const cricketContext = useContext(CricketContext);
+  const { changeType } = cricketContext;
+
   return (
     <div className="ph3">
       <div className="fl w-third pa2">
         <a
           className="f6 w-100 tc grow no-underline br-pill ph3 pv2 mb2 dib white bg-purple"
-          href="#0"
+          onClick={() => changeType(CONST.ALL)}
         >
           All
         </a>
@@ -14,7 +19,7 @@ const MatchType = () => {
       <div className="fl w-third pa2">
         <a
           className="f6 w-100 tc grow no-underline br-pill ph3 pv2 mb2 dib white bg-light-purple"
-          href="#0"
+          onClick={() => changeType(CONST.INTERNATIONAL)}
         >
           International
         </a>
@@ -22,7 +27,7 @@ const MatchType = () => {
       <div className="fl w-third pa2">
         <a
           className="f6 w-100 tc grow no-underline br-pill ph3 pv2 mb2 dib white bg-hot-pink"
-          href="#0"
+          onClick={() => changeType(CONST.DOMESTIC)}
         >
           Domestic
         </a>
