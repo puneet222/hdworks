1cfcd984b4e97cbea283481b592254a6940ddb36
puneet aggarwal <aggarwal.puneet222@gmail.com>
2019-10-13 14:46:11 +0530

    series classes

--- src/components/matchSeries/SeriesItem.js
+++ src/components/matchSeries/SeriesItem.js
@@ -14,27 +14,26 @@ const SeriesItem = ({ series }) => {
         })
       }
     >
-      <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">
+      <h1 className="f5 bg-near-white br3 br--top black-60 mv0 pv2 ph3">
         {series.seriesName}
-        {currentSeries}
       </h1>
       <div className="bt b--black-10">
-        <article className="pa3 pa5-ns" data-name="slab-stat-small">
-          <h3 className="f6 ttu tracked">{series.league}</h3>
+        <article className="pa3-l pa3-m pa3-ns" data-name="slab-stat-small">
+          <h3 className="f6 pl3 ttu tracked">{series.league}</h3>
           <div className="cf">
-            <dl className="fl fn-l w-third dib-l w-auto-l lh-title mr5-l">
+            <dl className="fl fn-l w-third dib-l lh-title">
               <dd className="f6 fw4 ml0 tc">ODI Matches</dd>
               <dd className="f3 fw6 ml0 tc">
                 {series.Odicount ? series.Odicount : 0}
               </dd>
             </dl>
-            <dl className="fl fn-l w-third dib-l w-auto-l lh-title mr5-l">
+            <dl className="fl fn-l w-third dib-l lh-title">
               <dd className="f6 fw4 ml0 tc">T20 Matches</dd>
               <dd className="f3 fw6 ml0 tc">
                 {series.T20count ? series.T20count : 0}
               </dd>
             </dl>
-            <dl className="fl fn-l w-third dib-l w-auto-l lh-title mr5-l">
+            <dl className="fl fn-l w-third dib-l lh-title">
               <dd className="f6 fw4 ml0 tc">Test Maches</dd>
               <dd className="f3 fw6 ml0 tc">
                 {series.Testcount ? series.Testcount : 0}
